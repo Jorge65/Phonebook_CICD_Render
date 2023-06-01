b01e80ddf25454ebc5ca7a40387eb64cd03f2ad1
Jorma Kemppainen <j.kemppainen@kolumbus.fi>
2023-06-01 12:47:17 +0300

    v1 test

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ var morgan = require('morgan')
 
 //let persons = [ ]
 
-console.log('...start...')
+console.log('...start. v1..')
 
 //app.use(morgan('tiny'))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :newperson'))
